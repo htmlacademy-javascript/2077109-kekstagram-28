e87428a837106f958bc547c4f08f80f55da71b9f
Keks <keks@htmlacademy.ru>
2023-03-09 11:10:58 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import { createArrayDescriptionsPhoto } from './data.js';
+import { createMiniatures } from './miniatures.js';
+import './data.js';
+import './util.js';
+import './miniatures.js';
+const pictures = createArrayDescriptionsPhoto();
+createMiniatures(pictures);
+
+
---
+++ 6/js/miniatures.js
@@ -0,0 +1,18 @@
+const template = document.querySelector('#picture').content;
+const pictureRenderingFragment = document.createDocumentFragment();
+const picturesContainer = document.querySelector('.pictures');
+
+const createMiniatures = (array) => {
+  array.forEach(({url, likes, comments}) => {
+    const userPictureElement = template.cloneNode(true);
+    userPictureElement.querySelector('.picture__likes').textContent = likes;
+    userPictureElement.querySelector('.picture__comments').textContent = comments.length;
+    userPictureElement.querySelector('.picture__img').src = url;
+    pictureRenderingFragment.appendChild(userPictureElement);
+  });
+
+  return picturesContainer.appendChild(pictureRenderingFragment);
+};
+
+export {createMiniatures};
+
